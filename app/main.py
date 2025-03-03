4089042afdbaa6e5f8d0f6dc0e0f390f23bb5503
Craig Hopgood <searaig@gmail.com>
2025-03-03 10:13:31 -0500

    run command via subprocess

--- app/main.py
+++ app/main.py
@@ -1,19 +1,61 @@
+from os.path import isdir
+import subprocess
 import sys
+import os
+
+
+def locate(os_path: str, filename: str) -> str | None:
+    for path in os_path.split(":"):
+        needle = f"{path}/{filename}"
+        if os.path.isfile(needle):
+            return needle
+    return None
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        cmd = input()
-        tok = cmd.split(" ")
+        istring = input()
+        tok = istring.split(" ")
+        cmd = tok[0]
+        args = tok[1:]
 
-        if tok[0] == "exit":
-            if len(tok) > 1 and tok[1].isdigit():
-                sys.exit(int(tok[1]))
+        # exit
+        if cmd == "exit":
+            if len(args) > 0 and args[0].isdigit():
+                sys.exit(int(args[0]))
             else:
                 sys.exit()
-        else:
+
+        # echo
+        if cmd == "echo":
+            sys.stdout.write(" ".join(args) + "\n")
+            continue
+
+        # type
+        if cmd == "type" and len(args) > 0:
+            for e in args:
+                if e == "echo":
+                    print("echo is a shell builtin")
+                elif e == "exit":
+                    print("exit is a shell builtin")
+                elif e == "type":
+                    print("type is a shell builtin")
+                else:
+                    cmdpath = locate(str(os.getenv("PATH")), args[0])
+                    if cmdpath is None:
+                        print(f"{cmd} is unknown")
+                    else:
+                        print(f"{cmd} is {cmdpath}")
+            continue
+
+        # attempt to execute
+        cmdpath = locate(str(os.getenv("PATH")), cmd)
+
+        if cmdpath is None:
             print(f"{cmd}: command not found")
+        else:
+            rs = subprocess.run(cmdpath, capture_output=True, text=True)
 
 
 if __name__ == "__main__":
